27576c26a4afe4531b33f9e76811b42e844d99fa
Usuario <Usuario@DESKTOP-A79ILPI>
2022-12-06 15:00:41 +0100

    segundo commit
    segundo commit

--- NuevoProyecto/src/nuevoproyecto/Nuevoproyecto.java
+++ NuevoProyecto/src/nuevoproyecto/Nuevoproyecto.java
@@ -5,6 +5,7 @@ public class Nuevoproyecto {
 	public static void main(String[] args) {
 		System.out.println("nuevo proyecto");
 		System.out.println("primer commit");
+		System.out.println("segfundo commit");
 	}
 
 }
