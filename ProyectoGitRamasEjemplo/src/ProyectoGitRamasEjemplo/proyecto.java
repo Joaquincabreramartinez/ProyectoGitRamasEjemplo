ba8ab9137bf5ab548b114987b99012a9b855a56a
Usuario <Usuario@DESKTOP-A79ILPI>
2022-12-06 12:31:55 +0100

    un commit del cambio (sin push)
    un commit del cambio (sin push)

--- ProyectoGitRamasEjemplo/src/ProyectoGitRamasEjemplo/proyecto.java
+++ ProyectoGitRamasEjemplo/src/ProyectoGitRamasEjemplo/proyecto.java
@@ -3,7 +3,7 @@ package ProyectoGitRamasEjemplo;
 public class proyecto {
 
 	public static void main(String[] args) {
-		System.out.println("hola mundo");
+		System.out.println("hello mundo");
 
 	}
 
