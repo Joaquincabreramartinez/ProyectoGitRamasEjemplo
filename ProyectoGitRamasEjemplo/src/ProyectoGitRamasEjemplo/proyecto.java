285f69e69c7c5a07319f9c60855e3d8cef51285a
Usuario <Usuario@DESKTOP-A79ILPI>
2022-12-06 12:53:05 +0100

    wwqaedw
    wdDDWS

--- ProyectoGitRamasEjemplo/src/ProyectoGitRamasEjemplo/proyecto.java
+++ ProyectoGitRamasEjemplo/src/ProyectoGitRamasEjemplo/proyecto.java
@@ -3,7 +3,7 @@ package ProyectoGitRamasEjemplo;
 public class proyecto {
 
 	public static void main(String[] args) {
-		System.out.println("saludos mundo");
+		System.out.println("salvudos mundo");
 
 	}
 	public static void imprimeSaludo() { 
