80af11747d97868f60b3a8d674005704b7bc3387
Usuario <Usuario@DESKTOP-A79ILPI>
2022-12-06 12:46:24 +0100

    wola en local u puss
    wola en local u puss

--- ProyectoGitRamasEjemplo/src/ProyectoGitRamasEjemplo/proyecto.java
+++ ProyectoGitRamasEjemplo/src/ProyectoGitRamasEjemplo/proyecto.java
@@ -3,11 +3,11 @@ package ProyectoGitRamasEjemplo;
 public class proyecto {
 
 	public static void main(String[] args) {
-		System.out.println("hello mundo");
+		System.out.println("saludos mundo");
 
 	}
 	public static void imprimeSaludo() { 
-		System.out.println(“Hei”);    
+		System.out.println("Hei");    
 	}
 
 }
